7541c3ea722fceca66571e439c998767bde3408a
Travis Ambrose <tmambrose@live.com>
2022-05-01 16:12:22 -0700

    Add router.post

--- simple-node-app/routes/index.js
+++ simple-node-app/routes/index.js
@@ -9,4 +9,8 @@ router.get('/', (req, res) => {
   res.render('form', { title: 'Registration form' });
 });
 
+router.post('/', (req, res) => {
+  res.render('form', { title: 'Registration form' });
+});
+
 module.exports = router;
