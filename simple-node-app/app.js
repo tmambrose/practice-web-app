d01b0938252597eda3087216af35e1fbc6f17537
tmambrose <tmambrose@live.com>
2022-05-01 13:34:53 +0000

    Add comments

--- simple-node-app/app.js
+++ simple-node-app/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const routes = require('./routes/index');
 
+// create a new express app
 const app = express();
+
+// whenever a request comes from forward slash
+// anything, use routes file
 app.use('/', routes);
 
 module.exports = app;
