ba6355735245650a307ecbe2fc1e412fa45e3414
tmambrose <tmambrose@live.com>
2022-05-01 13:29:17 +0000

    Add comments

--- simple-node-app/routes/index.js
+++ simple-node-app/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+// get router to respond to requests
 const router = express.Router();
 
+// root url response
 router.get('/', (req, res) => {
-  res.send('It works!');
+  res.send('It works! Okay!');
 });
 
 module.exports = router;
