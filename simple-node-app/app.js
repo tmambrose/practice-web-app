2adedea77dc7c4cd6183efdef5f38c4dcc6aa369
Travis Ambrose <tmambrose@live.com>
2022-05-01 07:09:42 -0700

    Add Pug functionality

--- simple-node-app/app.js
+++ simple-node-app/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/index');
 
 // create a new express app
 const app = express();
+
+// look for templates in views/
+// and use Pug as a layout engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // whenever a request comes from forward slash
 // anything, use routes file
