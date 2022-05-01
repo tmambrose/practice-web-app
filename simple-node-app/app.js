d66f331808d71281c0d48e3c3c7593ba9b4f84df
Travis Ambrose <tmambrose@live.com>
2022-05-01 16:14:51 -0700

    Add bodyParser to app

--- simple-node-app/app.js
+++ simple-node-app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
+const bodyParser = require('body-parser');
 
 // create a new express app
 const app = express();
@@ -12,6 +13,9 @@ app.set('view engine', 'pug');
 
 // whenever a request comes from forward slash
 // anything, use routes file
+app.use('/', routes);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 
 module.exports = app;
