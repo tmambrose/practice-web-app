2fdc52d4b96c5ae52f59e417370f45fd65f95080
tmambrose <tmambrose@live.com>
2022-05-02 00:21:41 +0000

    Add handler for database

--- simple-node-app/start.js
+++ simple-node-app/start.js
@@ -1,6 +1,17 @@
-const app = require('./app');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect($DATABASE, { useMongoClient: true });
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${$DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+const app = require('./app');
 // set up listener
 const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
